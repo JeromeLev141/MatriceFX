8dc7fca15d4daf25642868a62eaf3d29ae3e343b
jeremi_girard <girje1932987@etu.cegepjonquiere.ca>
2020-11-17 15:07:27 -0500

    matrice transposition

--- src/sample/Main2.java
+++ src/sample/Main2.java
@@ -9,14 +9,13 @@ public class Main2 {
     public static void main(String[] args) {
         Matrice a = GenererMatrice.genererMatrice(3,3);
         System.out.println(a.toString());
-        System.out.println(Operation.transposition(a));
-        /*Matrice b = GenererMatrice.genererMatrice(3,3);
+        Matrice b = GenererMatrice.genererMatrice(3,3);
         System.out.println(b.toString());
         Matrice c = Operation.addition(a,b);
         System.out.println(c.toString());
         Matrice d = Operation.soustraction(a,b);
         System.out.println(d.toString());
         Matrice e = Operation.multiplication(a, 2);
-        System.out.println(e.toString());*/
+        System.out.println(e.toString());
     }
 }
