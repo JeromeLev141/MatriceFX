c9c63bec50b8504bb15082648a0e3780acf4526a
levje1932644 <1932644@etu.cegepjonquiere.ca>
2020-11-17 11:21:34 -0500

    Projet final

--- src/sample/modele/Matrice.java
+++ src/sample/modele/Matrice.java
@@ -1,6 +1,32 @@
 package sample.modele;
 
+import java.util.List;
+
 public class Matrice {
 
-    private List;
+    private List<Double> elements;
+    private int m; //nombre de ligne
+    private int n; //nombre de colonne
+
+    public Matrice(int m, int n) {
+        this.m = m;
+        this.n = n;
+    }
+
+    public List<Double> getElements() { return elements; }
+
+    public void setElements(List<Double> elements) { this.elements = elements; }
+
+    public int getM() { return m; }
+
+    public int getN() { return n; }
+
+    @Override
+    public String toString() {
+        return "Matrice{" +
+                "elements=" + elements +
+                ", m=" + m +
+                ", n=" + n +
+                '}';
+    }
 }
