dd63badc935d142a2295a4dc458a4e907921ceb9
jeremi_girard <girje1932987@etu.cegepjonquiere.ca>
2020-11-17 12:13:27 -0500

    operation soustraction et multiplication

--- src/sample/controlleur/Operation.java
+++ src/sample/controlleur/Operation.java
@@ -8,7 +8,7 @@ public class Operation {
 
     }
 
-    static Matrice addition(Matrice a,Matrice b){
+    public static Matrice addition(Matrice a,Matrice b){
         if (verif1(a,b))
             for (int x=0 ; x<a.getElements().size();x++)
                 a.getElements().set(x, a.getElements().get(x)+b.getElements().get(x));
@@ -16,7 +16,7 @@ public class Operation {
         return a;
     }
 
-    static Matrice soustraction(Matrice a,Matrice b){
+    public static Matrice soustraction(Matrice a,Matrice b){
         if (verif1(a,b))
             for (int x=0 ; x<a.getElements().size();x++)
                 a.getElements().set(x, a.getElements().get(x)-b.getElements().get(x));
@@ -24,7 +24,7 @@ public class Operation {
         return a;
     }
 
-    static Matrice multiplication(Matrice a,double x){
+    public static Matrice multiplication(Matrice a,double x){
         for (int i = 0;i<a.getElements().size();i++)
             a.getElements().set(i, a.getElements().get(i)*x);
 
@@ -33,7 +33,7 @@ public class Operation {
 
 
 
-    static boolean verif1(Matrice matice1, Matrice matice2){
+    private static boolean verif1(Matrice matice1, Matrice matice2){
 
         if (matice1.getM() == matice2.getM() && matice1.getN() == matice2.getN() )
             return true;
