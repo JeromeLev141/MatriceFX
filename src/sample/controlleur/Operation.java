3950d43943ad22436d3439477a241f3f99df1a47
jeremi_girard <girje1932987@etu.cegepjonquiere.ca>
2020-11-17 11:48:38 -0500

    operation addition

--- src/sample/controlleur/Operation.java
+++ src/sample/controlleur/Operation.java
@@ -1,4 +1,26 @@
 package sample.controlleur;
 
+import sample.modele.Matrice;
+
 public class Operation {
+
+    public Operation() {
+
+    }
+
+    static Matrice addition(Matrice a,Matrice b){
+        if (verif1(a,b))
+            for (int x=0 ; x<a.getElements().size();x++)
+                a.getElements().set(x, a.getElements().get(x)+b.getElements().get(x));
+
+        return a;
+    }
+
+    static boolean verif1(Matrice matice1, Matrice matice2){
+
+        if (matice1.getM() == matice2.getM() && matice1.getN() == matice2.getN() )
+            return true;
+        else
+            return false;
+    }
 }
