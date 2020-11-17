4f3643fb692d053b5b71cd3f4710dbedc3d4ff70
levje1932644 <1932644@etu.cegepjonquiere.ca>
2020-11-17 14:29:59 -0500

    verif

--- src/sample/controlleur/Operation.java
+++ src/sample/controlleur/Operation.java
@@ -2,52 +2,40 @@ package sample.controlleur;
 
 import sample.modele.Matrice;
 
-import javax.swing.*;
-
 public class Operation {
 
     public Operation() {
 
     }
 
-    public static Matrice addition(Matrice a,Matrice b){
-        Matrice r = new Matrice(a.getM(), a.getN());
+    public static Matrice addition(Matrice a, Matrice b){
         if (verif1(a,b)) {
+            Matrice r = new Matrice(a.getM(), a.getN());
             for (int x = 0; x < a.getElements().size(); x++)
                 r.getElements().add(a.getElements().get(x) + b.getElements().get(x));
             return r;
         }
-        else
-            return null;
-
-
+        else return null;
     }
 
-    public static Matrice soustraction(Matrice a,Matrice b){
+    public static Matrice soustraction(Matrice a, Matrice b){
         if (verif1(a,b)) {
             Matrice r = new Matrice(a.getM(), a.getN());
             for (int x = 0; x < a.getElements().size(); x++)
                 r.getElements().add(a.getElements().get(x) - b.getElements().get(x));
             return r;
         }
-        else
-            return null;
-
-
+        else return null;
     }
 
-    public static Matrice multiplication(Matrice a,double x){
+    public static Matrice multiplication(Matrice a, double k){
         Matrice r = new Matrice(a.getM(), a.getN());
-        for (int i = 0;i<a.getElements().size();i++)
-            r.getElements().add(a.getElements().get(i) * x);
+        for (int x = 0; x < a.getElements().size(); x++)
+            r.getElements().add(a.getElements().get(x) * k);
         return r;
     }
 
-    private static boolean verif1(Matrice matice1, Matrice matice2){
-
-        if (matice1.getM() == matice2.getM() && matice1.getN() == matice2.getN() )
-            return true;
-        else
-            return false;
+    private static boolean verif1(Matrice a, Matrice b){
+        return a.getM() == b.getM() && a.getN() == b.getN();
     }
 }
