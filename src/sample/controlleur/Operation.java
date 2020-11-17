c5a74e5f98b0d87872cd9fa466d73737fbc378b8
jeremi_girard <girje1932987@etu.cegepjonquiere.ca>
2020-11-17 11:54:55 -0500

    operation soustraction et multiplication

--- src/sample/controlleur/Operation.java
+++ src/sample/controlleur/Operation.java
@@ -16,6 +16,23 @@ public class Operation {
         return a;
     }
 
+    static Matrice soustraction(Matrice a,Matrice b){
+        if (verif1(a,b))
+            for (int x=0 ; x<a.getElements().size();x++)
+                a.getElements().set(x, a.getElements().get(x)-b.getElements().get(x));
+
+        return a;
+    }
+
+    static Matrice multiplication(Matrice a,double x){
+        for (int i = 0;i<a.getElements().size();i++)
+            a.getElements().set(i, a.getElements().get(i)*x);
+
+        return a;
+    }
+
+
+
     static boolean verif1(Matrice matice1, Matrice matice2){
 
         if (matice1.getM() == matice2.getM() && matice1.getN() == matice2.getN() )
