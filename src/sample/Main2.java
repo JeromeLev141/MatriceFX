c5da89889cd68c303b402aaa8de5f090bffc2eb9
levje1932644 <1932644@etu.cegepjonquiere.ca>
2020-11-20 09:30:02 -0500

    Test unitaire

--- src/sample/Main2.java
+++ src/sample/Main2.java
@@ -12,6 +12,7 @@ public class Main2 {
         System.out.println(test.toString());
         System.out.println(test.estValide());
         Matrice a = GenererMatrice.genererMatrice(2,3);
+        System.out.println(a.getElements().toString());
         System.out.println(a.toString());
         System.out.println(a.estValide());
         System.out.println(Operation.listeFraction(a).toString());
