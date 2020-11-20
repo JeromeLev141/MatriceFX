039b5d20d58409ffaf288659647d596c925aacf3
levje1932644 <1932644@etu.cegepjonquiere.ca>
2020-11-20 08:24:04 -0500

    generer matrice

--- src/sample/controlleur/GenererMatrice.java
+++ src/sample/controlleur/GenererMatrice.java
@@ -9,11 +9,10 @@ public class GenererMatrice {
     public static Matrice genererMatrice(int m, int n) {
         Matrice matrice = new Matrice(m,n);
         Scanner sc = new Scanner(System.in);
-        for (int x = 1; x <= matrice.getM(); x++) {
-            for (int y = 1; y <= matrice.getN(); y++) {
-                System.out.println("Éléments a" + x + "" + y);
-                //matrice.getElements().add(sc.nextDouble());
-                matrice.setElement(x, y, sc.nextDouble());
+        for (m = 1; m <= matrice.getM(); m++) {
+            for (n = 1; n <= matrice.getN(); n++) {
+                System.out.println("Éléments a" + m + "" + n);
+                matrice.setElement(m, n, sc.nextDouble());
             }
         }
         return matrice;
