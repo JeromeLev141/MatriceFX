bf7aed311037558132685cde16a20582092b916e
jeremi_girard <girje1932987@etu.cegepjonquiere.ca>
2020-11-20 11:29:40 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Projet final/sample/Main2.class
    #	out/production/Projet final/sample/controlleur/Operation.class
    #	src/sample/controlleur/Operation.java

--- src/sample/Main2.java
+++ src/sample/Main2.java
@@ -4,17 +4,19 @@ import sample.controlleur.GenererMatrice;
 import sample.controlleur.Operation;
 import sample.modele.Matrice;
 
+import java.util.Objects;
+
 public class Main2 {
 
     public static void main(String[] args) {
 
-        Matrice test = new Matrice(2,2);
+        /*Matrice test = new Matrice(2,2);
         System.out.println(test.toString());
-        System.out.println(test.estValide());
-        Matrice a = GenererMatrice.genererMatrice(2,3);
+        System.out.println(test.estValide());*/
+        Matrice a = GenererMatrice.genererMatrice(4,4);
         System.out.println(a.toString());
         System.out.println(a.estValide());
-        System.out.println(Operation.listeFraction(a).toString());
+        /*System.out.println(Operation.listeFraction(a).toString());
         System.out.println(Operation.transposition(a).toString());
         Matrice b = GenererMatrice.genererMatrice(3,4);
         System.out.println(b.toString());
@@ -29,5 +31,8 @@ public class Main2 {
         Matrice g = Operation.produitVectoriel(a,b );
         if (g != null)
             System.out.println(g.toString());
+        */
+        double det1 = Objects.requireNonNull(Operation.determinant(a)).getElement(1, 1);
+        System.out.println(det1);
     }
 }
