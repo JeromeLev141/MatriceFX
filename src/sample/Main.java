2662942c601c369d8f9b3099c8e8ffde42578568
levje1932644 <1932644@etu.cegepjonquiere.ca>
2020-11-24 11:08:43 -0500

    Menu

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,19 +3,16 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import sample.mvc.modele.Matrice;
-import sample.mvc.vue.MatriceAffichage;
+import sample.mvc.vue.InterfaceUtilisateur;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        MatriceAffichage mat = new MatriceAffichage(new Matrice(3, 3), primaryStage);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(mat.afficherMatriceVide(), 1000, 600));
+        primaryStage.setScene(new InterfaceUtilisateur().getApplication());
         primaryStage.show();
     }
 
--- src/sample/mvc/vue/InterfaceUtilisateur.java
+++ src/sample/mvc/vue/InterfaceUtilisateur.java
@@ -1,4 +1,36 @@
 package sample.mvc.vue;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+
 public class InterfaceUtilisateur {
+
+    private Scene application;
+    private BorderPane bdp;
+
+    public InterfaceUtilisateur() {
+        bdp = new BorderPane();
+
+        Menu operations = new Menu("Opérations");
+        Menu fichier = new Menu("Fichier");
+
+        MenuItem addition = new MenuItem("Addition");
+        MenuItem soustraction = new MenuItem("Soustraction");
+        MenuItem multiplication = new MenuItem("multiplication par un scalaire");
+        MenuItem transposition = new MenuItem("Transposition");
+        MenuItem produitVectoriel = new MenuItem("Produit Vectoriel");
+        MenuItem determinant = new MenuItem("Calcul du déterminant");
+
+        operations.getItems().addAll(addition, soustraction, multiplication, transposition,
+                produitVectoriel, determinant);
+        MenuBar menuBar = new MenuBar(operations, fichier);
+        bdp.setTop(menuBar);
+
+        application = new Scene(bdp, 1000, 600);
+    }
+
+    public Scene getApplication() { return application; }
 }
