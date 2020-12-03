1389313153284700673f2dd747cbda9a483dadb6
levje1932644 <1932644@etu.cegepjonquiere.ca>
2020-12-02 20:08:48 -0500

    Scalaire affichage

---
+++ src/sample/mvc/vue/ScalaireAffichage.java
@@ -0,0 +1,30 @@
+package sample.mvc.vue;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+public class ScalaireAffichage extends HBox {
+
+    private int valeur;
+
+    public ScalaireAffichage() {
+        setAlignment(Pos.CENTER);
+        TextField scalaire = new TextField();
+        scalaire.setPromptText("K");
+        scalaire.setPrefColumnCount(2);
+        scalaire.setOnAction(event -> {
+            getChildren().remove(scalaire);
+            getChildren().add(Forme.genererScalaire(scalaire.getText()));
+            valeur = Integer.parseInt(scalaire.getText());
+        });
+        getChildren().add(scalaire);
+    }
+
+    public ScalaireAffichage(String valeur) {
+        getChildren().add(Forme.genererScalaire(valeur));
+        this.valeur = Integer.parseInt(valeur);
+    }
+
+    public int getValeur() { return valeur; }
+}
