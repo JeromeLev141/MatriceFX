94477525e344a29b55d5e5fde3950bf4883651e4
levje1932644 <1932644@etu.cegepjonquiere.ca>
2020-12-09 21:45:59 -0500

    ajout d'audios (non-terminé)

---
+++ src/sample/mvc/vue/Son.java
@@ -0,0 +1,24 @@
+package sample.mvc.vue;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+public class Son {
+
+    public static MediaPlayer erreurSon() {
+        return new MediaPlayer(new Media(
+                new File("src/sample/mvc/vue/audios/erreur.mp3").toURI().toString()));
+    }
+
+    public static MediaPlayer plusSon() {
+        return new MediaPlayer(new Media(
+                new File("src/sample/mvc/vue/audios/plus.mp3").toURI().toString()));
+    }
+
+    public static MediaPlayer moinsSon() {
+        return new MediaPlayer(new Media(
+                new File("src/sample/mvc/vue/audios/moins.mp3").toURI().toString()));
+    }
+}
